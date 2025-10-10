eeacf0808fac640aa4676bbd71a41d9dd77216cc
Harsh Agrawal <harsh.agrawal@lubrizol.com>
2025-10-10 13:28:37 +0530

    Title Text Component Added

--- ce-21-poc/src/app/page.tsx
+++ ce-21-poc/src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
-import styles from "./page.module.scss"
+import styles from "./page.module.scss";
+import ImageTitleText from './components/ImageTitleText/ImageTitleText';
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -20,7 +22,20 @@ export default function Home() {
           <Image width={320} height={480}  src="/images/3.jpg" alt="Art 3" />
           <h3>Abstract Art on Acrylic Color</h3>
         </div>
+         <div className={styles.artwork}>
+          <Image width={320} height={480} src="/images/1.jpg" alt="Art 1" />
+          <h3>Pen Art</h3>
+        </div>
+        <div className={styles.artwork}>
+          <Image width={320} height={480} src="/images/2.jpg" alt="Art 2" />
+          <h3>Spatula Work</h3>
+        </div>
+        <div className={styles.artwork}>
+          <Image width={320} height={480}  src="/images/3.jpg" alt="Art 3" />
+          <h3>Abstract Art on Acrylic Color</h3>
+        </div>
       </main>
+      <ImageTitleText imageUrl="/images/d.jpg" title="Meet the Artist" text="Loreum Epsum for Now"></ImageTitleText>
       <footer className={styles.footer}>
         <button>More Content Coming Soon!!!</button>
         <div>
